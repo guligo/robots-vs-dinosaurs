6505c9839e4ead4c5b976644d7adf79d52009391
Igors Gulbinskis <igors.gulbinskis@gmail.com>
2018-11-11 15:53:05 +0100

    #2: Adding Ring + Compojure libs

--- project.clj
+++ project.clj
@@ -1,3 +1,11 @@
 (defproject robots-vs-dinosaurs "0.1.0-SNAPSHOT"
   :description "Solution to Nubank's robots versus dinosaurs problem"
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :min-lein-version "2.0.0"
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [compojure "1.6.1"]
+                 [ring/ring-defaults "0.3.2"]]
+  :plugins [[lein-ring "0.12.4"]]
+  :ring {:handler com.nubank.exercise.handler/app}
+  :profiles
+  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
+                        [ring/ring-mock "0.3.2"]]}})
