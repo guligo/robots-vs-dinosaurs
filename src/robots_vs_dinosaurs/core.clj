cefdd2e620dbc09b840d886b4cc43ef1670fe70c
Igors Gulbinskis <igors.gulbinskis@gmail.com>
2018-11-11 13:39:18 +0100

    #1: Initial project structure

---
+++ src/robots_vs_dinosaurs/core.clj
@@ -0,0 +1,6 @@
+(ns robots-vs-dinosaurs.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/robots_vs_dinosaurs/core_test.clj
@@ -0,0 +1,7 @@
+(ns robots-vs-dinosaurs.core-test
+  (:require [clojure.test :refer :all]
+            [robots-vs-dinosaurs.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
