52c451b83cde1040eb86449752364e6c3fade503
Igors Gulbinskis <igors.gulbinskis@gmail.com>
2018-11-13 01:20:22 +0100

    #3: Initial get/update functions

--- src/com/nubank/exercise/core.clj
+++ src/com/nubank/exercise/core.clj
@@ -10,19 +10,39 @@
 
 (defn- private-create-actor [simulation, actor]
   "Adds an actor to simulation"
-  (Simulation. (conj (:actors simulation) actor)))
+  (if (not-any? #(= (:position %) (:position actor)) (:actors simulation))
+    (Simulation. (conj (:actors simulation) actor))
+    simulation))
 
-(defn- private-delete-actor [simulation, target-actor]
+(defn- private-delete-actor [simulation, actor]
   "Deletes an actor from simulation"
-  (Simulation. (remove (fn [actor] (= target-actor actor)) (:actors simulation))))
+  (Simulation. (remove #(= actor %) (:actors simulation))))
 
 (defn create-robot [simulation, robot]
   "Adds robot to simulation"
   (private-create-actor simulation robot))
 
+(defn get-robot [simulation, robot-id]
+  "Retrieves robot by its ID"
+  (some #(if (= robot-id (:id %)) %) (:actors simulation)))
+
 (defn create-dinosaur [simulation, dinosaur]
   "Adds dinosaur to simulation"
   (private-create-actor simulation dinosaur))
+
+(defn update-robot [simulation, robot]
+  "Updates robot in simulation based on its ID",
+  (def index
+    (first
+      (map
+        first
+        (filter
+          #(= (:id (second %)) (:id robot))
+          (map-indexed vector (:actors simulation))))))
+
+  (if (some? index)
+    (Simulation. (assoc (:actors simulation) index robot))
+    simulation))
 
 (defn delete-dinosaur [simulation, dinosaur]
   "Deletes dinosaur from simulation"
--- test/com/nubank/exercise/core_test.clj
+++ test/com/nubank/exercise/core_test.clj
@@ -3,14 +3,34 @@
             [com.nubank.exercise.core :refer :all])
   (:import (com.nubank.exercise.core Simulation Cell Robot Dinosaur)))
 
-(deftest adding-actors-to-simulation
+(deftest creating-actors-in-simulation
   (testing "Robot is added to simulation"
-    (is (= (Simulation. [(Robot. 1 (Cell. 0 0) :up)]) (create-robot (Simulation. []) (Robot. 1 (Cell. 0 0) :up)))))
+    (is (= (Simulation. [(Robot. 1 (Cell. 0 0) :up)])
+           (create-robot (Simulation. []) (Robot. 1 (Cell. 0 0) :up)))))
   (testing "Dinosaur is added to simulation"
-    (is (= (Simulation. [(Dinosaur. (Cell. 0 0))]) (create-dinosaur (Simulation. []) (Dinosaur. (Cell. 0 0)))))))
+    (is (= (Simulation. [(Dinosaur. (Cell. 0 0))])
+           (create-dinosaur (Simulation. []) (Dinosaur. (Cell. 0 0))))))
+  (testing "Robot is not added to simulation if an actor already exists in that spot"
+    (is (= (Simulation. [(Dinosaur. (Cell. 0 0))])
+           (create-robot (Simulation. [(Dinosaur. (Cell. 0 0))]) (Robot. 1 (Cell. 0 0) :up))))))
+
+(deftest retrieving-actors-from-simulation
+  (testing "Robot is retrieved from simulation by its ID"
+    (is (= (Robot. 2 (Cell. 1 1) :down)
+           (get-robot (Simulation. [(Robot. 1 (Cell. 0 0) :up), (Robot. 2 (Cell. 1 1) :down), (Robot. 3 (Cell. 2 2) :left)]) 2)))))
+
+(deftest updating-actors-in-simulation
+  (testing "Robot is updated if robot with existing ID is found"
+    (is (= (Simulation. [(Robot. 1 (Cell. 0 1) :down)]),
+           (update-robot (Simulation. [(Robot. 1 (Cell. 0 0) :right)]) (Robot. 1 (Cell. 0 1) :down)))))
+  (testing "Robot is not updated if robot with existing ID is not found"
+    (is (= (Simulation. [(Robot. 1 (Cell. 0 0) :right)]),
+           (update-robot (Simulation. [(Robot. 1 (Cell. 0 0) :right)]) (Robot. 2 (Cell. 0 1) :down))))))
 
 (deftest deleting-actors-from-simulation
   (testing "Dinosaur is removed from simulation"
-    (is (= (Simulation. []) (delete-dinosaur (Simulation. [(Dinosaur. (Cell. 0 0))]) (Dinosaur. (Cell. 0 0))))))
+    (is (= (Simulation. [])
+           (delete-dinosaur (Simulation. [(Dinosaur. (Cell. 0 0))]) (Dinosaur. (Cell. 0 0))))))
   (testing "Robot is not removed from simulation when actually dinosaur is expected to be removed"
-    (is (= (Simulation. [(Robot. 1 (Cell. 0 0) :up)]) (delete-dinosaur (Simulation. [(Robot. 1 (Cell. 0 0) :up)]) (Dinosaur. (Cell. 0 0)))))))
+    (is (= (Simulation. [(Robot. 1 (Cell. 0 0) :up)])
+           (delete-dinosaur (Simulation. [(Robot. 1 (Cell. 0 0) :up)]) (Dinosaur. (Cell. 0 0)))))))
