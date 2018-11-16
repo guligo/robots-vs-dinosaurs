f8c47b4949295a1dc63662f29fede9309addfe0e
Igors Gulbinskis <igors.gulbinskis@gmail.com>
2018-11-16 09:24:07 +0100

    #6: Persist app state

--- src/com/nubank/exercise/rest.clj
+++ src/com/nubank/exercise/rest.clj
@@ -5,15 +5,18 @@
             [compojure.handler :as handler]
             [ring.middleware.json :as middleware]))
 
+(def simulation (atom (->Simulation [])))
+
 (defroutes api-routes
-  (GET    "/simulation" []   {:status 200, :body (->Simulation[])})
-  (DELETE "/simulation" []   {:status 204})
-  (POST   "/robots"     []   {:status 204})
-  (PATCH  "/robots/:id" [id] {:status 204})
-  (POST   "/dinosaurs"  []   {:status 204})
-  (route/not-found           {:status 404}))
+  (GET    "/simulation" [] {:status 200, :body @simulation})
+  (DELETE "/simulation" [] (reset! simulation (->Simulation [])) {:status 204})
+  (POST   "/robots"     [robot] {:status 204})
+  (PATCH  "/robots/:id" [id] (println id) {:status 204})
+  (POST   "/dinosaurs"  {dino :body} (println dino) {:status 204})
+  (route/not-found      {:status 404}))
 
 (def app
   (->
     (handler/site api-routes)
+    (middleware/wrap-json-body)
     (middleware/wrap-json-response)))
