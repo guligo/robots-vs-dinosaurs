9eb63a52abedbe2b8c150d357dfba0745d969671
Igors Gulbinskis <igors.gulbinskis@gmail.com>
2018-11-17 00:13:47 +0100

    #6: Fix formatting and unit-tests

--- src/com/nubank/exercise/core.clj
+++ src/com/nubank/exercise/core.clj
@@ -1,5 +1,4 @@
-(ns com.nubank.exercise.core
-  (:import (com.sun.xml.internal.bind.v2.model.core ID)))
+(ns com.nubank.exercise.core)
 
 (defrecord Cell [row, col])
 
@@ -11,28 +10,28 @@
 
 (defn- private-create-actor [simulation, actor]
   "Adds an actor to simulation"
-  (if (not-any? #(and (= (:pos :row %) (:pos :row actor)) (= (:pos :col %) (:pos :col actor))) (:actors simulation))
+  (if (not-any? #(and (= (:row (:pos %)) (:row (:pos actor))) (= (:col (:pos %)) (:col (:pos actor)))) (:actors simulation))
     (->Simulation (conj (:actors simulation) actor))
     simulation))
 
 (defn- private-delete-actor [simulation, actor]
-  " Deletes an actor from simulation "
+  "Deletes an actor from simulation "
   (->Simulation (remove #(= actor %) (:actors simulation))))
 
 (defn create-robot [simulation, robot]
-  " Adds robot to simulation "
+  "Adds robot to simulation "
   (private-create-actor simulation robot))
 
 (defn get-robot [simulation, robot-id]
-  " Retrieves robot by its ID "
+  "Retrieves robot by its ID "
   (some #(if (= robot-id (:id %)) %) (:actors simulation)))
 
 (defn create-dinosaur [simulation, dinosaur]
-  " Adds dinosaur to simulation "
+  "Adds dinosaur to simulation "
   (private-create-actor simulation dinosaur))
 
 (defn update-robot [simulation, robot]
-  " Updates robot in simulation based on its ID ",
+  "Updates robot in simulation based on its ID ",
   (def index
     (first
       (map
@@ -46,7 +45,7 @@
     simulation))
 
 (defn delete-dinosaur [simulation, dinosaur]
-  " Deletes dinosaur from simulation "
+  "Deletes dinosaur from simulation "
   (if (instance? Dinosaur dinosaur)
     (private-delete-actor simulation dinosaur)
     simulation))
