3549600572131425e635a1e95e32b28b9e6fd817
Igors Gulbinskis <igors.gulbinskis@gmail.com>
2018-11-17 00:06:54 +0100

    #6: Fix position comparison bug

--- src/com/nubank/exercise/core.clj
+++ src/com/nubank/exercise/core.clj
@@ -1,4 +1,5 @@
-(ns com.nubank.exercise.core)
+(ns com.nubank.exercise.core
+  (:import (com.sun.xml.internal.bind.v2.model.core ID)))
 
 (defrecord Cell [row, col])
 
@@ -10,28 +11,28 @@
 
 (defn- private-create-actor [simulation, actor]
   "Adds an actor to simulation"
-  (if (not-any? #(= (:pos %) (:pos actor)) (:actors simulation))
+  (if (not-any? #(and (= (:pos :row %) (:pos :row actor)) (= (:pos :col %) (:pos :col actor))) (:actors simulation))
     (->Simulation (conj (:actors simulation) actor))
     simulation))
 
 (defn- private-delete-actor [simulation, actor]
-  "Deletes an actor from simulation"
+  " Deletes an actor from simulation "
   (->Simulation (remove #(= actor %) (:actors simulation))))
 
 (defn create-robot [simulation, robot]
-  "Adds robot to simulation"
+  " Adds robot to simulation "
   (private-create-actor simulation robot))
 
 (defn get-robot [simulation, robot-id]
-  "Retrieves robot by its ID"
+  " Retrieves robot by its ID "
   (some #(if (= robot-id (:id %)) %) (:actors simulation)))
 
 (defn create-dinosaur [simulation, dinosaur]
-  "Adds dinosaur to simulation"
+  " Adds dinosaur to simulation "
   (private-create-actor simulation dinosaur))
 
 (defn update-robot [simulation, robot]
-  "Updates robot in simulation based on its ID",
+  " Updates robot in simulation based on its ID ",
   (def index
     (first
       (map
@@ -45,7 +46,7 @@
     simulation))
 
 (defn delete-dinosaur [simulation, dinosaur]
-  "Deletes dinosaur from simulation"
+  " Deletes dinosaur from simulation "
   (if (instance? Dinosaur dinosaur)
     (private-delete-actor simulation dinosaur)
     simulation))
--- src/com/nubank/exercise/rest.clj
+++ src/com/nubank/exercise/rest.clj
@@ -11,7 +11,7 @@
   (GET    "/simulation" [] {:status 200, :body @simulation})
   (DELETE "/simulation" [] (reset! simulation (->Simulation [])) {:status 204})
   (POST   "/robots"     {robot :body} (reset! simulation (create-robot @simulation robot)) {:status 204})
-  (PATCH  "/robots/:id" [id] (println id) {:status 204})
+  (PATCH  "/robots/:id" [id] {:status 204})
   (POST   "/dinosaurs"  {dinosaur :body} (reset! simulation (create-dinosaur @simulation dinosaur)) {:status 204})
   (route/not-found      {:status 404}))
 
