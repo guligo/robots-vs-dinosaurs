26a6c829277289b86a59105114c4b51be88f6aee
Igors Gulbinskis <igors.gulbinskis@gmail.com>
2018-11-25 12:39:23 +0100

    #17: Modify conditions within action

--- src/com/nubank/exercise/models/robot.clj
+++ src/com/nubank/exercise/models/robot.clj
@@ -7,53 +7,47 @@
   ([row, col, dirn, id] (assoc (actor :robot row col id) :dirn dirn)))
 
 (defn action
-  ([name] {:name name})
-  ([name, param] {:name name :param param}))
+  ([name] {:action name})
+  ([name, param] {:action name :param param}))
 
 (defn create-robot [actors, robot]
   "Adds robot to simulation"
   (create-actor actors robot))
 
-(defn attack-dinosaurs [actors, robot]
+(defn- attack-dinosaurs [actors, robot]
   "Destroys dinosaurs around robot"
   (-> actors
-      (delete-actor :dinosaur (- (:row robot) 1) (:col robot))
-      (delete-actor :dinosaur (+ (:row robot) 1) (:col robot))
-      (delete-actor :dinosaur (:row robot) (- (:col robot) 1))
-      (delete-actor :dinosaur (:row robot) (+ (:col robot) 1))))
+      (delete-actor :dinosaur (dec (:row robot)) (:col robot))
+      (delete-actor :dinosaur (inc (:row robot)) (:col robot))
+      (delete-actor :dinosaur (:row robot) (dec (:col robot)))
+      (delete-actor :dinosaur (:row robot) (inc (:col robot)))))
 
-(defn turn-robot [actors, robot, side]
+(defn- turn-robot [actors, robot, side]
   "Turns robot to the left or right"
-  (let [dirn (if (= side :left)
-               (case (:dirn robot)
-                 :north :west
-                 :west :south
-                 :south :east
-                 :east :north)
-               (if (= side :right)
-                 (case (:dirn robot)
-                   :north :east
-                   :east :south
-                   :south :west
-                   :west :north)
-                 side))]
-    (update-actor actors (assoc robot :dirn dirn))))
+  (let [dirn (cond
+               (and (= side :left) (= (:dirn robot) :north)) :west
+               (and (= side :left) (= (:dirn robot) :west)) :south
+               (and (= side :left) (= (:dirn robot) :south)) :east
+               (and (= side :left) (= (:dirn robot) :east)) :north
+               (and (= side :right) (= (:dirn robot) :north)) :east
+               (and (= side :right) (= (:dirn robot) :east)) :south
+               (and (= side :right) (= (:dirn robot) :south)) :west
+               (and (= side :right) (= (:dirn robot) :west)) :north)]
+    (if (some? dirn)
+      (update-actor actors (assoc robot :dirn dirn))
+      actors)))
 
-(defn move-robot [actors, robot, motion]
+(defn- move-robot [actors, robot, motion]
   "Moves robot forward or backward"
-  (let [move (if (= motion :forward)
-               (case (:dirn robot)
-                 :north {:row (- (:row robot) 1)}
-                 :west {:col (- (:col robot) 1)}
-                 :south {:row (+ (:row robot) 1)}
-                 :east {:col (+ (:col robot) 1)})
-               (if (= motion :backward)
-                 (case (:dirn robot)
-                   :north {:row (- (:row robot) 1)}
-                   :west {:col (+ (:col robot) 1)}
-                   :south {:row (+ (:row robot) 1)}
-                   :east {:col (- (:col robot) 1)})
-                 {}))]
+  (let [move (cond
+               (and (= motion :forward) (= (:dirn robot) :north)) {:row (dec (:row robot))}
+               (and (= motion :forward) (= (:dirn robot) :west)) {:col (dec (:col robot))}
+               (and (= motion :forward) (= (:dirn robot) :south)) {:row (inc (:row robot))}
+               (and (= motion :forward) (= (:dirn robot) :east)) {:col (inc (:col robot))}
+               (and (= motion :backward) (= (:dirn robot) :north)) {:row (inc (:row robot))}
+               (and (= motion :backward) (= (:dirn robot) :west)) {:col (inc (:col robot))}
+               (and (= motion :backward) (= (:dirn robot) :south)) {:row (dec (:row robot))}
+               (and (= motion :backward) (= (:dirn robot) :east)) {:col (dec (:col robot))})]
     (update-actor actors (merge robot move))))
 
 (defn perform-robot-action [actors, robot-id, action]
@@ -61,8 +55,8 @@
   (let [robot (get-actor actors :robot robot-id)
         name (:action action)
         param (:param action)]
-    (case name
-      :attack (attack-dinosaurs actors robot)
-      :turn (turn-robot actors robot param)
-      :move (move-robot actors robot param)
-      actors)))
+    (cond
+      (and (some? robot) (= name :attack)) (attack-dinosaurs actors robot)
+      (and (some? robot) (= name :turn)) (turn-robot actors robot param)
+      (and (some? robot) (= name :move)) (move-robot actors robot param)
+      :else actors)))
--- test/com/nubank/exercise/models/robot_test.clj
+++ test/com/nubank/exercise/models/robot_test.clj
@@ -11,59 +11,111 @@
 
 (facts "About attack"
        (fact "Attack destroys dinosaurs in front, to the left, to the right or behind"
-             (attack-dinosaurs [(dinosaur 0 0 0)
-                                (dinosaur 0 1 1)
-                                (dinosaur 0 2 2)
-                                (dinosaur 2 0 3)
-                                (dinosaur 2 1 4)
-                                (dinosaur 2 2 5)
-                                (dinosaur 1 0 6)
-                                (robot 1 1 :south 7)
-                                (dinosaur 1 2 8)]
-                               (robot 1 1 :south 7))
+             (perform-robot-action [(dinosaur 0 0 0)
+                                    (dinosaur 0 1 1)
+                                    (dinosaur 0 2 2)
+                                    (dinosaur 2 0 3)
+                                    (dinosaur 2 1 4)
+                                    (dinosaur 2 2 5)
+                                    (dinosaur 1 0 6)
+                                    (robot 1 1 :south 7)
+                                    (dinosaur 1 2 8)]
+                                   7
+                                   (action :attack))
              => [(dinosaur 0 0 0)
                  (dinosaur 0 2 2)
                  (dinosaur 2 0 3)
                  (dinosaur 2 2 5)
                  (robot 1 1 :south 7)])
        (fact "Attack does not destroy robot"
-             (attack-dinosaurs [(robot 0 1 :west 0)
-                                (dinosaur 1 0 1)
-                                (robot 1 1 :east 2)]
-                               (robot 1 1 :east 2))
+             (perform-robot-action [(robot 0 1 :west 0)
+                                    (dinosaur 1 0 1)
+                                    (robot 1 1 :east 2)]
+                                   2
+                                   (action :attack))
              => [(robot 0 1 :west 0)
                  (robot 1 1 :east 2)])
        (fact "Attack does not fail if performed on the border"
-             (attack-dinosaurs [(dinosaur 0 0 0)
-                                (dinosaur 0 1 1)
-                                (robot 1 0 :east 2)
-                                (dinosaur 1 1 3)
-                                (dinosaur 2 0 4)
-                                (dinosaur 2 1 5)]
-                               (robot 1 0 :east 2))
+             (perform-robot-action [(dinosaur 0 0 0)
+                                    (dinosaur 0 1 1)
+                                    (robot 1 0 :east 2)
+                                    (dinosaur 1 1 3)
+                                    (dinosaur 2 0 4)
+                                    (dinosaur 2 1 5)]
+                                   2
+                                   (action :attack))
              => [(dinosaur 0 1 1)
                  (robot 1 0 :east 2)
                  (dinosaur 2 1 5)])
        (fact "Attack does not fail if performed when no actor is around"
-             (attack-dinosaurs [(robot 49 49 :north 0)]
-                               (robot 49 49 :north 0))
-             => [(robot 49 49 :north 0)]))
+             (perform-robot-action [(robot 49 49 :north 0)] 0 (action :attack))
+             => [(robot 49 49 :north 0)])
+       (fact "Attack cannot be performed by dinosaur"
+             (perform-robot-action [(dinosaur 0 1 0) (dinosaur 1 1 1)] 1 (action :attack))
+             => [(dinosaur 0 1 0) (dinosaur 1 1 1)])
+       (fact "Attack cannot be performed by nonexistent robot"
+             (perform-robot-action [(robot 0 0 0)] 1 (action :attack))
+             => [(robot 0 0 0)]))
 
-(facts "About turning robot"
-       (fact "Possible to turn robot left"
-             (turn-robot [(robot 0 0 :north 0)] (robot 0 0 :north 0) :left)
-             => [(robot 0 0 :west 0)])
-       (fact "Possible to turn robot right"
-             (turn-robot [(robot 5 5 :east 1)] (robot 5 5 :east 1) :right)
-             => [(robot 5 5 :south 1)]))
+(facts "About turning"
+       (fact "Robot needs parameter in order to turn"
+             (perform-robot-action [(robot 1 2 :north 0)] 0 (action :turn))
+             => [(robot 1 2 :north 0)])
+       (fact "Robot can turn left"
+             (perform-robot-action [(robot 10 20 :north 3)] 3 (action :turn :left))
+             => [(robot 10 20 :west 3)]
+             (perform-robot-action [(robot 10 20 :west 3)] 3 (action :turn :left))
+             => [(robot 10 20 :south 3)]
+             (perform-robot-action [(robot 10 20 :south 3)] 3 (action :turn :left))
+             => [(robot 10 20 :east 3)]
+             (perform-robot-action [(robot 10 20 :east 3)] 3 (action :turn :left))
+             => [(robot 10 20 :north 3)])
+       (fact "Robot can turn right"
+             (perform-robot-action [(robot 5 5 :east 1)] 1 (action :turn :right))
+             => [(robot 5 5 :south 1)]
+             (perform-robot-action [(robot 5 5 :south 1)] 1 (action :turn :right))
+             => [(robot 5 5 :west 1)]
+             (perform-robot-action [(robot 5 5 :west 1)] 1 (action :turn :right))
+             => [(robot 5 5 :north 1)]
+             (perform-robot-action [(robot 5 5 :north 1)] 1 (action :turn :right))
+             => [(robot 5 5 :east 1)])
+       (fact "Dinosaur cannot turn"
+             (perform-robot-action [(dinosaur 0 0 0)] 0 (action :turn :right))
+             => [(dinosaur 0 0 0)])
+       (fact "Nonexistent robot cannot turn"
+             (perform-robot-action [(robot 0 0 0)] 1 (action :turn :right))
+             => [(robot 0 0 0)]))
 
 (facts "About moving robot"
-       (fact "Possible to move robot"
-             (move-robot [(robot 24 24 :north 123)] (robot 24 24 :north 123) :forward)
-             => [(robot 23 24 :north 123)])
-       (fact "Impossible to move robot outside of border"
-             (move-robot [(robot 0 0 :west 321)] (robot 0 0 :west 321) :forward)
+       (fact "Robot needs parameter in order to move"
+             (perform-robot-action [(robot 1 2 :north 0)] 0 (action :move))
+             => [(robot 1 2 :north 0)])
+       (fact "Robot can move"
+             (perform-robot-action [(robot 24 24 :north 123)] 123 (action :move :forward))
+             => [(robot 23 24 :north 123)]
+             (perform-robot-action [(robot 24 24 :east 123)] 123 (action :move :forward))
+             => [(robot 24 25 :east 123)]
+             (perform-robot-action [(robot 24 24 :south 123)] 123 (action :move :forward))
+             => [(robot 25 24 :south 123)]
+             (perform-robot-action [(robot 24 24 :west 123)] 123 (action :move :forward))
+             => [(robot 24 23 :west 123)]
+             (perform-robot-action [(robot 24 24 :north 123)] 123 (action :move :backward))
+             => [(robot 25 24 :north 123)]
+             (perform-robot-action [(robot 24 24 :east 123)] 123 (action :move :backward))
+             => [(robot 24 23 :east 123)]
+             (perform-robot-action [(robot 24 24 :south 123)] 123 (action :move :backward))
+             => [(robot 23 24 :south 123)]
+             (perform-robot-action [(robot 24 24 :west 123)] 123 (action :move :backward))
+             => [(robot 24 25 :west 123)])
+       (fact "Robot cannot move outside of border"
+             (perform-robot-action [(robot 0 0 :west 321)] 321 (action :move :forward))
              => [(robot 0 0 :west 321)])
-       (fact "Impossible to move robot in place occupied by other actor"
-             (move-robot [(robot 24 24 :north 123) (dinosaur 25 24 321)] (robot 24 24 :south 123) :backward)
-             => [(robot 24 24 :north 123) (dinosaur 25 24 321)]))
+       (fact "Robot cannot move in place occupied by other actor"
+             (perform-robot-action [(robot 24 24 :north 123) (dinosaur 25 24 321)] 123 (action :move :backward))
+             => [(robot 24 24 :north 123) (dinosaur 25 24 321)])
+       (fact "Dinosaur cannot move"
+             (perform-robot-action [(dinosaur 0 0 0)] 0 (action :move :forward))
+             => [(dinosaur 0 0 0)])
+       (fact "Nonexistent robot cannot move"
+             (perform-robot-action [(robot 0 0 0)] 1 (action :move :forward))
+             => [(robot 0 0 0)]))
