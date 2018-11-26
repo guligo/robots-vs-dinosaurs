a88ad615b8b400332611619e4fd2d7c518ff4491
Igors Gulbinskis <igors.gulbinskis@gmail.com>
2018-11-26 23:06:06 +0100

    #12: Fix bug

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject robots-vs-dinosaurs "1.0.0"
   :description "Solution to Nubank's robots versus dinosaurs problem"
   :min-lein-version "2.0.0"
+  :jvm-opts ["-Xmx256m"]
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [metosin/compojure-api "1.1.11"]
                  [hiccup "1.0.5"]
@@ -12,11 +13,9 @@
                                                     com.sun.jmx/jmxri]]]
   :plugins [[lein-ring "0.12.4"]]
   :ring {:handler com.nubank.exercise.app/app}
-  :profiles
-
-  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                        [ring/ring-mock "0.3.2"]
-                        [midje "1.9.4"]]
-         :plugins [[lein-midje "3.2.1"]
-                   [lein-cloverage "1.0.13"]
-                   [lein-codox "0.10.5"]]}})
+  :profiles {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
+                                  [ring/ring-mock "0.3.2"]
+                                  [midje "1.9.4"]]
+                   :plugins [[lein-midje "3.2.1"]
+                             [lein-cloverage "1.0.13"]
+                             [lein-codox "0.10.5"]]}})
