cb7ae8b100cbdb731e47f3b18472f729cc675020
Igors Gulbinskis <igors.gulbinskis@gmail.com>
2018-12-11 23:55:58 +0100

    #23: Minor improvements

--- test/com/nubank/exercise/models/simulation_test.clj
+++ test/com/nubank/exercise/models/simulation_test.clj
@@ -1,36 +1,37 @@
 (ns com.nubank.exercise.models.simulation-test
   (:require [midje.sweet :refer :all]
-            [com.nubank.exercise.models.simulation :refer :all]
             [com.nubank.exercise.models.robot :refer :all]
-            [com.nubank.exercise.models.dinosaur :refer :all]))
+            [com.nubank.exercise.models.dinosaur :refer :all]
+            [com.nubank.exercise.models.simulation :refer :all]))
 
 (facts "About simulation"
-       (background (after :facts (delete-simulation!)))
+       (background (after :facts (delete-actors!)))
        (fact "Simulation persists state"
              (do (create-robot! (robot 0 0 :north))
-                 (get-simulation))
-             => (simulation [(robot 0 0 :north 0)]))
+                 (get-actors))
+             => [(robot 0 0 :north 0)])
        (fact "Possible to reset ongoing simulation"
              (do (create-robot! (robot 0 0 :north))
-                 (delete-simulation!))
-             => (simulation-status true))
+                 (delete-actors!)
+                 (get-actors)
+             => []))
        (fact "Dinosaur creation is delegated"
              (do (create-dinosaur! (dinosaur 3 3))
-                 (get-simulation))
-             => (simulation [(dinosaur 3 3 321)])
+                 (get-actors))
+             => [(dinosaur 3 3 321)]
              (provided (create-dinosaur [] (let [dinosaur (dinosaur 3 3)] dinosaur))
                        => [(dinosaur 3 3 321)] :times 1))
        (fact "Robot creation is delegated"
              (do (create-robot! (robot 1 1 :east))
-                 (get-simulation))
-             => (simulation [(robot 1 1 :east 123)])
+                 (get-actors))
+             => [(robot 1 1 :east 123)]
              (provided (create-robot [] (let [robot (robot 1 1 :east)] robot))
                        => [(robot 1 1 :east 123)] :times 1))
        (fact "Robot action is delegated"
              (do (create-robot! (robot 3 3 :west))
                  (perform-robot-action! 0 (action :move :forward))
-                 (get-simulation))
-             => (simulation [(robot 3 2 :west 0)])
+                 (get-actors))
+             => [(robot 3 2 :west 0)]
              (provided (perform-robot-action [(robot 3 3 :west 0)]
                                              0
                                              (let [action (action :move :forward)] action)) => [(robot 3 2 :west 0)] :times 1)))
